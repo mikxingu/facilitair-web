0c1147ffafc6c403db9fc7af620de42e6fc95f6b
Mike Alves <michelalvs@gmail.com>
2023-01-14 21:07:26 -0300

    Working Connection with DB

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
+from src.routers import stocks
+
+### SQL
+
+
 
 app = FastAPI()
 
 @app.get("/")
 async def get_root():
     return "Hello!"
+
+app.include_router(stocks.router)
---
+++ src/routers/stocks.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Request, Depends, BackgroundTasks
+from requests import Session
+
+###SQL
+import sqlite3 as sql
+database = sql.connect('./src/database/database.db')
+
+def get_stocks_db():
+
+    with database:
+        cursor = database.cursor()
+        cursor.execute("SELECT ticker FROM stocks ")
+        stocks = cursor.fetchall()
+        return stocks
+    
+
+router = APIRouter(prefix="/stocks", tags=['stocks'])
+
+@router.get("")
+async def get_all_stocks():
+    """
+    Recupera todos os tickers cadastrados no banco
+    """
+    tickers = get_stocks_db()
+    return tickers
+
+@router.get("/{ticker}")
+async def get_all_stocks(ticker: str):
+    """
+    Retorna informações da ação para o ticker informado.
+    """
+    # TODO - CRIAR SERVIÇO NO BANCO PARA FAZER UMA QUERY PARA UMA AÇÃO ESPECÍFICA.
+    return ticker   
