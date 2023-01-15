b32c436d58b52cb8e8ee487b4e73153d40d9ea74
Mike Alves <michelalvs@gmail.com>
2023-01-14 21:28:39 -0300

    Fixed Everything for deploy

--- src/routers/stocks.py
+++ src/routers/stocks.py
@@ -1,17 +1,16 @@
-from fastapi import APIRouter, Request, Depends, BackgroundTasks
-from requests import Session
+from fastapi import APIRouter
 
 ###SQL
-# import sqlite3 as sql
-# database = sql.connect('./src/database/database.db')
+import sqlite3 as sql
+database = sql.connect('./src/database/database.db')
 
-# def get_stocks_db():
+def get_stocks_db():
 
-#     with database:
-#         cursor = database.cursor()
-#         cursor.execute("SELECT ticker FROM stocks ")
-#         stocks = cursor.fetchall()
-#         return stocks
+    with database:
+        cursor = database.cursor()
+        cursor.execute("SELECT ticker FROM stocks ")
+        stocks = cursor.fetchall()
+        return stocks
     
 
 router = APIRouter(prefix="/stocks", tags=['stocks'])
@@ -21,8 +20,7 @@ async def get_all_stocks():
     """
     Recupera todos os tickers cadastrados no banco
     """
-    # tickers = get_stocks_db()
-    tickers = ["AABC3"]
+    tickers = get_stocks_db()
     return tickers
 
 @router.get("/{ticker}")
